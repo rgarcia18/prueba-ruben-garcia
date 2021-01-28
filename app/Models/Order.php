445eb96ba0e579c82dfd7cb558d76d03ef452055
rgarcia18 <ruga-18@hotmail.com>
2021-01-27 21:36:52 -0500

    feat: returns the name of the order status

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -19,4 +19,23 @@ class Order extends Model
     function product(){
         return $this->hasOne(\App\Models\Product::class,'id','product_id');
     }//product
+    
+    /**
+     * Returns the order status
+     * @param string $status Order Status
+     * @return string Status name
+     */
+    function getStatus(){
+        
+        $names = [
+            'APPROVED' => trans('orders.approved'),
+            'CREATED' => trans('orders.created'),
+            'PAYED' => trans('orders.payed'),
+            'REJECTED' => trans('orders.rejected'),
+            'PENDING' => trans('orders.pending'),
+        ];
+        
+        return $this->status ? $names[$this->status] : trans('orders.undefined');
+        
+    }//status
 }
