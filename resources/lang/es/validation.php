6e0a36cf552bbd86ca1985ff707210a456fe848e
rgarcia18 <ruga-18@hotmail.com>
2021-01-27 21:17:32 -0500

    feat: constants in english and spanish for validation messages

---
+++ resources/lang/es/validation.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+     | ------------------------------------------------- -------------------------
+     | Líneas de validación de idioma
+     | ------------------------------------------------- -------------------------
+     |
+     | Las siguientes líneas de idioma contienen los mensajes de error predeterminados utilizados por
+     | la clase de validador. Algunas de estas reglas tienen múltiples versiones como
+     | como las reglas de tamaño. Siéntase libre de modificar cada uno de estos mensajes aquí.
+    |
+    */
+
+    'required' => 'El campo :attribute es requerido.',
+    'email' => 'El :attribute debe ser una dirección de correo electrónico válida.',
+    'max' => [
+        'string' => 'El campo :attribute no puede superar los :max caracteres',
+    ],
+];
