94c570cf936f18ebcaeab9c0a6be7a5b5bb2f54e
rgarcia18 <ruga-18@hotmail.com>
2021-01-27 21:12:26 -0500

    feat: constants for the english and spanish languages of the home page

---
+++ resources/lang/en/home.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'my_orders' => 'My orders',
+    'home' => 'Home',
+    'services' => 'Services',
+    'contact' => 'Contact',
+    'store_name' => 'Sports shop',
+    
+];
---
+++ resources/lang/es/home.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'my_orders' => 'Mis órdenes',
+    'home' => 'Inicio',
+    'services' => 'Servicios',
+    'contact' => 'Contacto',    
+    'store_name' => 'Tienda Deportiva',
+    
+];
